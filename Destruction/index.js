2e5c42d0ba1a1318b7660d5fda0ed0d074b34de3
Yevgeny <unstoo@gmail.com>
2018-06-11 20:44:09 +0300

    Object destruction

--- Destruction/index.js
+++ Destruction/index.js
@@ -1,6 +1,25 @@
-const o = {
-  name: 42
+const city = {
+  name: 'St.Petersburg',
+  coordinates: {
+    N: '59.9343°', E: '30.3351°'
+  }
 }
 
-console.log(o);
+const { name, coordinates: { N }, coordinates } = city
 
+name
+N
+coordinates
+
+const langs = [
+  { name: 'C++' },
+  { name: 'PHP' },
+  { name: 'JavaScript' },
+  { name: 'Python' },
+  { name: 'C#' },
+  { name: 'Ruby' }
+]
+
+const {2: JS} = langs
+
+JS
